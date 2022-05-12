376acefe509fbd4c3621862054335b2d7867331f
dylst <cdylan127@gmail.com>
2022-05-11 22:13:58 -0700

    Render lists and dynamic styles

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-## Expenses Tracker
+# Expenses Tracker
+
+This is an application that lets you input expenses, and compare them by month throughout a selected year.<br>
+This project was completed following the "React - The Complete Guide (incl Hooks, React Router, Redux)" course by Maximilian Schwarzmuller.<br>
+
+## Outcome of this project:
+
+Learned some React fundamentals including:
+
+<li> What is JSX </li>
+<li> What are Components </li>
+<li> What is a component tree and how it works </li>
+<li> What are props </li>
+<li> Passing data between components </li>
+<li> Output dynamic data in React components </li>
+<li> Event listeners in react </li>
+<li> The importance of state </li>
+<li> The useState hook </li>
+<li> Updating a state </li>
+<li> Lifting the state up </li>
+<li> Rendering lists of data </li>
+<li> Outputting conditional content </li>
+<li> Adding dynamic styles </li>
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
@@ -6,42 +6,45 @@ import Expenses from "./components/Expenses/Expenses";
 const DUMMY_EXPENSES = [
   {
     id: "e1",
-    title: "New Sweatshirt",
-    amount: 78.12,
-    date: new Date(2020, 7, 14),
+    title: "Pillows",
+    amount: 94.12,
+    date: new Date(2020, 6, 12),
   },
   {
     id: "e2",
-    title: "Car Insurance",
-    amount: 254.45,
-    date: new Date(2021, 2, 28),
+    title: "New Monitor",
+    amount: 499.49,
+    date: new Date(2021, 2, 12),
   },
   {
     id: "e3",
-    title: "New Monitor",
-    amount: 502.38,
-    date: new Date(2021, 2, 12),
+    title: "Car Insurance",
+    amount: 298.67,
+    date: new Date(2021, 2, 28),
   },
   {
     id: "e4",
     title: "New Desk",
-    amount: 450.0,
-    date: new Date(2021, 5, 13),
+    amount: 350.0,
+    date: new Date(2021, 5, 15),
   },
 ];
 
-function App() {
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={DUMMY_EXPENSES} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
